1768f74a84475b0df4e03f6621ba9d6e7aeaa78c
zhanghaoran <2017929959@qq.com>
2022-12-27 09:50:39 +0800

    ""

---
+++ leetcode2027.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int ans = 0;
+        for (int i = 0;i < s.size();i ++) {
+            if (s[i] == 'X')
+                ans ++,i += 2;
+        }
+        return ans;
+    }
+};
+int main() {
+
+    return 0;
+}
