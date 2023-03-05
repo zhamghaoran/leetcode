9b9e2acb45c8963f00a7c47a83cce7f1614e3184
zhanghaoran <2017929959@qq.com>
2023-03-05 09:55:23 +0800

    ""

--- Acwing2058.cpp
+++ Acwing2058.cpp
@@ -1,32 +1,68 @@
 #include "bits/stdc++.h"
-using namespace std;
-int main() {
-    string a,b;
-    cin>>a>>b;
-    set<int> list;
-    for (int i = a.size() - 1;i >= 0;i --) {
-        int ans = 0;
-        for (int j = a.size();j >= 0;j --) {
-            if (j == i) {
-                ans <<= !(a[j] - '0');
-            } else ans <<= (a[j] - '0');
-        }
-        list.insert(ans);
-    }
-    for (int i = b.size() - 1;i >= 0;i --) {
-        for (int j = b.size() - 1;j >= 0;j --) {
-            int ans = 0;
-            int cheng = 1;
-            int x = b[j] - '0';
-            if (i == j) {
-                if (x + 1 <= 2) {
-                    ans += cheng * (x + 1);
-                }
-                if (x + )
-            } else {
 
+using namespace std;
+map<int, int> faq;
+map<int, int> faqq;
+char three[3] = {'0', '1', '2'};
+
+int minn(int x, unsigned int y) {
+    return x < y ? x : y;
+}
+
+void twototen(string x) {
+    int ans = 0;
+    for (char i: x) {
+        ans *= 2;
+        ans += i - '0';
+
+    }
+    faq[ans]++;
+}
+
+void threetoten(string x) {
+    int ans = 0;
+    for (char i: x) {
+        ans *= 3;
+        ans += i - '0';
+    }
+    faqq[ans]++;
+}
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+    int mo = a.size() - 1;
+    while (a[mo] == '0') mo--;
+    for (int i = 0; i <= minn(mo + 1, a.size() - 1); i ++) {
+        string x = a;
+        if (x[i] == '1' && i == 0)
+            continue;
+        if (x[i] == '1')
+            x[i] = '0';
+        else
+            x[i] = '1';
+        twototen(x);
+    }
+    mo = b.size() - 1;
+    while (b[mo] == '0')
+        mo--;
+    for (int i = 0; i <= minn(mo + 1, b.size() - 1); i ++) {
+        for (char j: three) {
+            if (i == 0 && j == '0')
+                continue;
+            string x = b;
+            if (x[i] != j) {
+                x[i] = j;
+                threetoten(x);
             }
         }
     }
+    std::for_each(faq.begin(), faq.end(), [&](pair<const int, int> item) {
+        if (item.second == 1 && faqq[item.first] == 1) {
+            cout << item.first;
+            exit(0);
+        }
+    });
+    cout<<0;
     return 0;
 }
