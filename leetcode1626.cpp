308d75848127886801d17947259f9b40be6c30cf
zhanghaoran <2017929959@qq.com>
2023-03-22 13:35:43 +0800

    ""

--- leetcode1626.cpp
+++ leetcode1626.cpp
@@ -4,33 +4,57 @@ using namespace std;
 
 class Solution {
 public:
-    int bestTeamScore(vector<int> &scores, vector<int> &ages) {
+    vector<int> tree;
+    int maxx;
+
+    int lowbit(int x) {
+        return x & (-x);
+    }
+
+    void update(int x, int val) {
+        for (; x <= maxx; x += lowbit(x))
+            tree[x] = max(tree[x], val);
+    }
+
+    int get(int x) {
         int ans = 0;
+        for (; x > 0; x -= lowbit(x))
+            ans = max(ans, tree[x]);
+        return ans;
+    }
+
+    int bestTeamScore(vector<int> &scores, vector<int> &ages) {
+        int ans;
+        std::for_each(scores.begin(), scores.end(), [&](const auto &item) {
+            maxx = max(maxx, item);
+        });
+
+        tree = vector<int>(maxx + 10, 0);
         vector<pair<int, int>> faq;
         for (int i = 0; i < scores.size(); i++)
             faq.emplace_back(scores[i], ages[i]);
+
         std::sort(faq.begin(), faq.end(), [&](pair<int, int> x, pair<int, int> y) -> bool {
             if (x.second == y.second)
                 return x.first < y.first;
             return x.second < y.second;
         });
-        vector<int> dp(scores.size() + 10, 0);
-        dp[0] = faq[0].first;
+
+        update(faq[0].first, faq[0].first);
+        ans = faq[0].first;
+
         for (int i = 1; i < faq.size(); i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (faq[j].first <= faq[i].first)
-                    dp[i] = max(dp[j] + faq[i].first, dp[i]);
-            }
-            dp[i] = max(dp[i], faq[i].first);
-            ans = max(ans,dp[i]);
+            int qaq = get(faq[i].first);
+            update(faq[i].first, qaq + faq[i].first);
+            ans = max(qaq + faq[i].first, ans);
         }
         return ans;
     }
 };
 
 int main() {
-    vector<int> score = {9,2,8,8,2};
-    vector<int> age = {4,1,3,3,5};
+    vector<int> score = {1,2,3,5};
+    vector<int> age = {8,9,10,1};
     cout << (new Solution)->bestTeamScore(score, age);
     return 0;
 }
