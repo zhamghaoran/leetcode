2de56e972f48a2219d644cb12f906644b3352eb9
jjking <2017929959@qq.com>
2023-09-08 07:53:22 +0800

    ""

---
+++ leetcode2651.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        return (arrivalTime + delayedTime) % 24;
+
+    }
+};
+
+int main() {
+
+    return 0;
+}
