09010230ba478ad4eef2c46fbb193d4e3216fc43
jjking <2017929959@qq.com>
2023-09-07 16:38:55 +0800

    ""

--- meituan1.cpp
+++ meituan1.cpp
@@ -1,29 +1,48 @@
 #include <bits/stdc++.h>
 
-#define int long long
 using namespace std;
+set<string> faq;
+map<string, vector<string>> name;
+map<string, int> nums;
+map<string, string> zhudian;
 
+bool pdxioaxie(string s) {
+    for (char i : s) {
+        if (i > 'z' || i < 'a')
+            return false;
+    }
+    return true;
+}
 
-signed main() {
+int main() {
     int a;
     cin >> a;
-    vector<int> val(a + 1);
-    int faq = 0;
     for (int i = 1; i <= a; i++) {
-        scanf("%lld", &val[i]);
-
-        faq += val[i];
-    }
-    int maxx = 0;
-    int sum = 0x3f3f3f3f;
-    for (int i = 2; i <= a; i++) {
-        if (val[i] * val[i - 1] > maxx) {
-            maxx = val[i] * val[i - 1];
-            sum = val[i] + val[i - 1];
-        } else if (val[i] * val[i - 1] == maxx) {
-            sum = min(sum, val[i] + val[i - 1]);
+        string x, y;
+        cin >> x >> y;
+        if (!pdxioaxie(x) || !pdxioaxie(y)) {
+            continue;
+        }
+        if (!faq.count(x)) {
+            faq.insert(x);
+            vector<string> na{y};
+            name[x] = na;
+            zhudian[x] = y;
+        } else {
+            vector<string> na = name[x];
+            if (std::find(na.begin(), na.end(), y) == na.end()) {
+                na.push_back(y);
+                name[x] = na;
+                nums[x]++;
+            }
         }
     }
-    cout << faq - sum + maxx;
+    vector<pair<string, vector<string>>> faq(name.begin(), name.end());
+    std::sort(faq.begin(), faq.end(), [&](const auto a, const auto b) -> bool {
+        return a.first < b.first;
+    });
+    for (const auto& i: faq) {
+        cout << i.first << " " << zhudian[i.first] << " " << nums[i.first]<<endl;
+    }
     return 0;
 }
