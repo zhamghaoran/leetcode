7de62dde032f5464c58b7ff324c53107eb4d9092
jjking <2017929959@qq.com>
2023-10-17 12:23:20 +0800

    ""

---
+++ leetcode2652.cpp
@@ -0,0 +1,19 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int ans = 0;
+        for (int i = 1;i <= n;i ++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+                ans += i;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
